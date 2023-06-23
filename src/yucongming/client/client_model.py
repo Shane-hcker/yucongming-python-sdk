434713526f07b759193216bfa87103b95019bb34
Shane <shanebilibili@outlook.com>
2023-06-23 23:34:53 +0800

    some bugfixes

--- src/yucongming/client/client_model.py
+++ src/yucongming/client/client_model.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
+import datetime
 import hashlib
 import requests
+from random import sample
 
 from .chat.crequest import *
 from .chat.cresponse import *
@@ -15,18 +17,27 @@ class YCMClient:
     def getHeaderDict(self, body) -> Dict[str, str]:
         if not (self.accessKey or self.secretKey):
             raise InterruptedError('access key or secret key hasn\'t been setup yet')
+        e_body = hashlib.md5(body.encode('utf-8')).hexdigest()
+        date = f"{datetime.datetime.now().timestamp():.0f}"
         return dict(
             accessKey=self.accessKey,
-            body=hashlib.md5(body.encode('utf-8')).hexdigest(),
-            sign=self.getSignature(body, self.secretKey)
+            sign=self.getSignature(e_body, self.secretKey),
+            body=e_body,
+            nonce=str(sample(range(1, 10), 4)),
+            timestamp=date,
         )
 
     @staticmethod
     def getSignature(body, secret_key) -> str:
-        return hashlib.sha256(f'{body}.{secret_key}'.encode('utf-8')).hexdigest()
+        stuff = hashlib.sha256(f'{body}.{secret_key}'.encode('utf-8')).hexdigest()
+        return stuff
 
     def start_chat(self, crequest: DevChatRequest) -> DevChatResponse:
         json_ = crequest.toJsonString()
         url = f'{self.host}/chat'
-        res = requests.post(url, headers=self.getHeaderDict(json_))
+        headers = self.getHeaderDict(json_)
+        # headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        #                               'Chrome/86.0.4240.198 Safari/537.36'})
+        print(headers)
+        res = requests.post(url, headers=headers)
         return DevChatResponse(res.content)
